aa846c1a1ba1b07bcb73e517e32c6ba3a2ba834c
Piero Revolledo <63229235@certus.edu.pe>
2024-08-19 23:10:54 -0500

    fhg

--- catalog.js
+++ catalog.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const items = [
         { id: 1, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/1.jpg' },
-        { id: 2, type: 'Tumba', name: 'Tumba Doble', img: 'https://via.placeholder.com/150' },
+        { id: 2, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/2.jpg' },
+        { id: 3, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/3.jpg' },
+        { id: 4, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/4.jpg' },
+        { id: 5, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/5.jpg' },
+        { id: 6, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/6.jpg' },
+        { id: 7, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/7.jpg' },
+        { id: 8, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/8.jpg' },
+        { id: 9, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/9.jpg' },
+        { id: 10, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/10.jpg' },
+        { id: 11, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/11.jpg' },
+        { id: 12, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/12.jpg' },
+        { id: 13, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/13.jpg' },
+        { id: 14, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/14.jpg' },
+        { id: 15, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/15.jpg' },
+        { id: 16, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/16.jpg' },
+        { id: 17, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/17.jpg' },
+        { id: 18, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/18.jpg' },
+        { id: 19, type: 'Lápida', name: 'Lápida Clásica', img: 'img/Imgcatalogo/19.jpg' },
+        { id: 20, type: 'Tumba', name: 'Tumba Doble', img: 'img/Imgcatalogo/20.jpg' },
         // Añadir más ítems...
     ];
 
