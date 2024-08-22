89a5d756ecf6d01d8eec63a15510653f31caf4e8
Piero Revolledo <63229235@certus.edu.pe>
2024-08-21 21:27:14 -0500

    gjh

--- catalog.js
+++ catalog.js
@@ -123,3 +123,9 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+const carousel = document.querySelector('#carouselExample');
+const carouselInstance = new bootstrap.Carousel(carousel, {
+    interval: 3000,
+    ride: 'carousel'
+});
